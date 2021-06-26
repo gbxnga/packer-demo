8daa4b3e67fc518709022e2cee859e659b01cebb
Gbenga Oni <30432941+gbxnga@users.noreply.github.com>
2021-06-26 18:18:35 +0100

    version 2 update

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! This is an update.');
+  res.send('Hello World! This is an update. V2');
 });
 
 var server = app.listen(3000, function () {
